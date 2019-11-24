9242781fc6caa96d942721e8c53c1e71918b56b9
Sudhanshu Ranjan <perfectsudh@gmail.com>
2019-11-24 19:08:23 +0530

    added upvote subscription

--- server/src/resolvers/Link.js
+++ server/src/resolvers/Link.js
@@ -2,6 +2,11 @@ const postedBy = (parent, args, context) => (
     context.prisma.link({ id: parent.id }).postedBy()
 )
 
+const votes = (parent, args, context) => (
+    context.prisma.link({ id: parent.id }).votes()
+)
+
 module.exports = {
     postedBy,
+    votes,
 }
--- server/src/resolvers/Subscription.js
+++ server/src/resolvers/Subscription.js
@@ -2,11 +2,21 @@ const newLinkSubscribe = (parent, args, context, info) => (
     context.prisma.$subscribe.link({ mutation_in: ["CREATED"] }).node()
 )
 
+const newVoteSubscription = (parent, args, context, info) => (
+    context.prisma.$subscribe.vote({ mutation_in: ["CREATED"] }).node()
+)
+
 const newLink = {
     subscribe: newLinkSubscribe,
     resolve: payload => payload,
 }
 
+const newVote = {
+    subscribe: newVoteSubscription,
+    resolve: payload => payload,
+}
+
 module.exports = {
-    newLink
+    newLink,
+    newVote,
 }
