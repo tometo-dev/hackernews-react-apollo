a7ce2e20be9f394b5639aa61d4f7f132bea38cc9
Sudhanshu Ranjan <perfectsudh@gmail.com>
2019-11-24 19:12:46 +0530

    added filter in queries

--- server/src/resolvers/Query.js
+++ server/src/resolvers/Query.js
@@ -1,6 +1,15 @@
-const feed = (parent, args, context, info) => (
-    context.prisma.links()
-)
+const feed = async (parent, args, context, info) => {
+    const where = args.filter ? {
+        OR: [
+            { description_contains: args.filter },
+            { url_contains: args.filter },
+        ],
+    } : {}
+    const links = await context.prisma.links({
+        where
+    })
+    return links
+}
 
 module.exports = {
     feed,
