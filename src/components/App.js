b010cf117025bf63f2803cbdbe1f3fc8aeecef3b
Sudhanshu Ranjan <perfectsudh@gmail.com>
2019-11-26 05:57:46 +0530

    checkpoint reached `Mutations: Creating Links`

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React from "react"
 
 import LinkList from "./LinkList"
+import CreateLink from "./CreateLink"
 
 function App() {
   return (
-    <LinkList />
+    <CreateLink />
   )
 }
 
