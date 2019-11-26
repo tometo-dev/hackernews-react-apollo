38623e28c268f120917ea8491dc5506936ed1f10
Sudhanshu Ranjan <perfectsudh@gmail.com>
2019-11-26 08:40:17 +0530

    checkpoint `routes` completed

--- src/components/CreateLink.js
+++ src/components/CreateLink.js
@@ -42,7 +42,11 @@ const CreateLink = (props) => {
                     placeholder="The URL for the link"
                 />
             </div>
-            <Mutation mutation={POST_MUTATION} variables={{ description, url }}>
+            <Mutation
+                mutation={POST_MUTATION}
+                variables={{ description, url }}
+                onCompleted={() => props.history.push("/")}
+            >
                 {(postMutation) => (
                     <button onClick={postMutation}>
                         Submit
